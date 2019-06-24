a153cb71f81959865d9205b0e18414ba3635dc1b
Alejandro Orozco Builes <aorozcobuiles@gmail.com>
2019-06-24 10:56:34 -0500

    Initial Commit

---
+++ Pokemon/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Pokemon
+//
+//  Created by Alejandro Orozco Builes on 24/06/19.
+//  Copyright © 2019 Alejandro Orozco Builes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
